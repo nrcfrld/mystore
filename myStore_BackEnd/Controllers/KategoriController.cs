34ae9e8b8c63f8f9908a1a8d37a5898a286a62a8
Enrico Feraldo Kalengkongang <nrcfrld@gmail.com>
2020-10-30 16:33:26 +0700

    Add Kategori Index & Users Index

---
+++ myStore_BackEnd/Controllers/KategoriController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myStore_BackEnd.Models;
+
+namespace myStore_BackEnd.Controllers
+{
+    public class KategoriController : Controller
+    {
+        mystoreEntities db = new mystoreEntities();
+
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            ViewData["categories"] = db.kategoris.ToList();
+            ViewData["breadcrumbs"] = "Dashboard / Kategori";
+
+            return View();
+        }
+    }
+}
---
+++ myStore_BackEnd/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myStore_BackEnd.Models;
+
+namespace myStore_BackEnd.Controllers
+{
+    public class UserController : Controller
+    {
+        mystoreEntities db = new mystoreEntities();
+        // GET: Users
+        public ActionResult Index()
+        {
+            ViewData["users"] = db.users.ToList();
+            ViewData["breadcrumbs"] = "Dashboard / Users";
+            return View();
+        }
+    }
+}
